57916ceb454beaaa4b362dc38f4f1378e5b1f2c2
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-17 22:26:10 +0200

    Define body for RequestInterface

--- src/Common/Request/RequestInterface.php
+++ src/Common/Request/RequestInterface.php
@@ -4,4 +4,11 @@ namespace Slickpay\Common\Request;
 
 interface RequestInterface
 {
+    public function method(): string;
+
+    public function endpoint(): string;
+
+    public function headers(): array;
+
+    public function body();
 }
