0d9f3ba5cb848c0788eb914aaacfc9508bf70e05
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-24 16:36:57 +0200

    Add coments for AbstractRequest

--- src/Common/Request/AbstractRequest.php
+++ src/Common/Request/AbstractRequest.php
@@ -7,6 +7,11 @@ use Slickpay\Common\Request\RequestInterface;
 
 abstract class AbstractRequest implements RequestInterface
 {
+    /**
+     * PSR-7 request instance.
+     * 
+     * @var \Psr\Http\Message\RequestInterface 
+     */
     public \Psr\Http\Message\RequestInterface $request;
 
     public function __construct()
@@ -14,6 +19,11 @@ abstract class AbstractRequest implements RequestInterface
         $this->request = new Request($this->method(), $this->endpoint(), $this->headers(), \json_encode($this->body()));
     }
 
+    /**
+     * Returns an instance of PSR-7 request.
+     * 
+     * @return \Psr\Http\Message\RequestInterface
+     */
     public function request(): \Psr\Http\Message\RequestInterface
     {
         return $this->request;
