2973554891c13c1d215c5a539e8a4218e859dcf4
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-24 16:31:42 +0200

    Add coments for ResponseInterface

--- src/Common/Response/ResponseInterface.php
+++ src/Common/Response/ResponseInterface.php
@@ -2,11 +2,28 @@
 
 namespace Slickpay\Common\Response;
 
+use Psr\Http\Message\MessageInterface;
+
 interface ResponseInterface
 {
+    /**
+     * Handler method for post-request actions.
+     * 
+     * @return ResponseInterface 
+     */
     public function update(): self;
 
-    public function response(): \Psr\Http\Message\MessageInterface;
+    /**
+     * PSR-7 response instance.
+     * 
+     * @return MessageInterface 
+     */
+    public function response(): MessageInterface;
 
+    /**
+     * Helper method to access body of request.
+     * 
+     * @return mixed 
+     */
     public function getBody();
 }
