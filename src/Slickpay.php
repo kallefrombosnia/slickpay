4448f970da00d7a1c852dcaf17fa4c83da08c9c8
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-24 16:41:06 +0200

    Add coments for Slickpay

--- src/Slickpay.php
+++ src/Slickpay.php
@@ -2,6 +2,7 @@
 
 namespace Slickpay;
 
+use Psr\Http\Client\ClientExceptionInterface;
 use Psr\Http\Client\ClientInterface;
 use Slickpay\Common\Gateway\GatewayInterface;
 use Slickpay\Common\Request\RequestInterface;
@@ -9,10 +10,25 @@ use Slickpay\Common\Response\ResponseInterface;
 
 class Slickpay
 {
+    /**
+     * Instance of Slickpay gateway.
+     *
+     * @var GatewayInterface
+     */
     protected GatewayInterface $gateway;
 
+    /**
+     * Instance of the Slickpay request.
+     *
+     * @var RequestInterface
+     */
     protected RequestInterface $request;
 
+    /**
+     * Instance of PSR-18 HTTP client.
+     *
+     * @var ClientInterface
+     */
     protected ClientInterface $client;
 
     public function __construct()
@@ -20,6 +36,12 @@ class Slickpay
         $this->client = new \GuzzleHttp\Client();
     }
 
+    /**
+     * Set & construct gateway class.
+     *
+     * @param string $gateway
+     * @return Slickpay
+     */
     public function setGateway(string $gateway): self
     {
         $this->gateway = new $gateway();
@@ -27,11 +49,22 @@ class Slickpay
         return $this;
     }
 
+    /**
+     * Returns an instance of gateway.
+     *
+     * @return GatewayInterface
+     */
     public function getGateway(): GatewayInterface
     {
         return $this->gateway;
     }
 
+    /**
+     * Set & construct request class.
+     *
+     * @param string $request
+     * @return Slickpay
+     */
     public function setRequest(string $request): self
     {
         $this->request = new $request();
@@ -39,11 +72,22 @@ class Slickpay
         return $this;
     }
 
+    /**
+     * Returns an instance of request.
+     *
+     * @return RequestInterface
+     */
     public function getRequest(): RequestInterface
     {
         return $this->request;
     }
 
+    /**
+     * Set an instance of custom PSR-18 HTTP client.
+     *
+     * @param ClientInterface $client
+     * @return Slickpay
+     */
     public function setClient(ClientInterface $client): self
     {
         $this->client = $client;
@@ -51,11 +95,22 @@ class Slickpay
         return $this;
     }
 
+    /**
+     * Returns an instance of HTTP client.
+     *
+     * @return ClientInterface
+     */
     public function getClient(): ClientInterface
     {
         return $this->client;
     }
 
+    /**
+     * Trigger request sending.
+     *
+     * @return ResponseInterface
+     * @throws ClientExceptionInterface
+     */
     public function send(): ResponseInterface
     {
         $response = $this->client->sendRequest(
